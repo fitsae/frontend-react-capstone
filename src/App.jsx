9bdd315013e2f891065c6a01de3263cf25c81528
fitsum <fitsaemagna@gmail.com>
2026-09-19 19:25:31 +0100

    wire all routes with catch-all

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Home";
+import Requests from "./requests/Requests";
+import RequestDetail from "./requests/RequestDetail";
+import DonorRegistration from "./donors/DonorRegistration";
+import Login from "./auth/Login";
+import About from "./About";
+import NotFound from "./NotFound";
+
+export default function App() {
   return (
-    <>
-      <h1>welcome to lifedrop project</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="requests" element={<Requests />} />
+          <Route path="requests/:id" element={<RequestDetail />} />
+          <Route path="donate" element={<DonorRegistration />} />
+          <Route path="login" element={<Login />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-export default App;
